5e70cf1e18c029be1f2e3042c5aa948c3448a172
connected-jyeung <jyeung@connected.io>
2019-01-14 11:48:50 -0500

    JY - Created helper functions to check if a UILabel is truncated or not

--- PieceOfCake_SampleApp/PieceOfCake_SampleAppTests/SampleAppSpec.swift
+++ PieceOfCake_SampleApp/PieceOfCake_SampleAppTests/SampleAppSpec.swift
@@ -10,6 +10,7 @@ class SampleAppSpec: QuickSpec {
         var nibSubject : NibViewController!
         var actionSubject : ActionViewController!
         var overlapSubject : OverlapViewController!
+        var labelSubject: LabelViewController!
         
         describe("ViewController from storyboard") {
             beforeEach() {
@@ -60,7 +61,7 @@ class SampleAppSpec: QuickSpec {
         
         describe("ViewController from nib") {
             beforeEach() {
-                nibSubject = UIViewController.loadAndSetupViewControllerFromNib("NibViewController", Bundle.main, NibViewController.self)
+                nibSubject = UIViewController.loadAndSetupViewControllerFromNib("NibViewController", NibViewController.self)
             }
 
             it("should not be nil") {
@@ -101,7 +102,7 @@ class SampleAppSpec: QuickSpec {
         
         describe("ViewController with user inputs") {
             beforeEach() {
-                actionSubject = UIViewController.loadAndSetupViewControllerFromNib("ActionViewController", Bundle.main, ActionViewController.self)
+                actionSubject = UIViewController.loadAndSetupViewControllerFromNib("ActionViewController", ActionViewController.self)
             }
             
             it("should not be nil") {
@@ -166,7 +167,7 @@ class SampleAppSpec: QuickSpec {
         
         describe("View Controller with overlapping elements") {
             beforeEach {
-                overlapSubject = UIViewController.loadAndSetupViewControllerFromNib("OverlapViewController", Bundle.main, OverlapViewController.self)
+                overlapSubject = UIViewController.loadAndSetupViewControllerFromNib("OverlapViewController", OverlapViewController.self)
             }
             
             context("When two views are overlapped and are within subviews") {
@@ -194,6 +195,36 @@ class SampleAppSpec: QuickSpec {
                     expect(overlapSubject.imageView.findOverlappedArea(with: overlapSubject.redButton)).to(equal(CGRect(x: 16.0, y: 478.0, width: 30.0, height: 13.0)))
                     expect(overlapSubject.imageView.findOverlappedArea(with: overlapSubject.blueLabel)).to(equal(CGRect(x: 46.0, y: 478.0, width: 42.0, height: 9.0)))
                     expect(overlapSubject.imageView.findOverlappedArea(with: overlapSubject.greenLabel)).to(equal(CGRect(x: 87.0, y: 478.0, width: 27.0, height: 9.0)))
+                }
+            }
+        }
+        
+        describe("View Controller with untruncated and truncated labels") {
+            beforeEach {
+                labelSubject = UIViewController.loadAndSetupViewControllerFromNib("LabelViewController", LabelViewController.self)
+            }
+            
+            context("when the labels are set") {
+                it("should find the untruncated labels") {
+                    expect(labelSubject.oneLineLabelNoTruncation.isTruncated()).to(equal(false))
+                    expect(labelSubject.twoLineLabelNoTruncation.isTruncated()).to(equal(false))
+                    expect(labelSubject.infiniteLineLabel.isTruncated()).to(equal(false))
+                }
+                
+                it("should find the truncated labels") {
+                    expect(labelSubject.oneLineLabelWithTruncation.isTruncated()).to(equal(true))
+                    expect(labelSubject.oneLineLabelWithIncreasingFontToTruncate.isTruncated()).to(equal(true))
+                    expect(labelSubject.oneLineLabelThatSizeWillDecreaseToTruncate.isTruncated()).to(equal(true))
+                    expect(labelSubject.threeLineLabelWithTruncation.isTruncated()).to(equal(true))
+                }
+                
+                it("should determine the theoretical number of lines the label should have to look untruncated") {
+                    expect(labelSubject.oneLineLabelNoTruncation.numberOfTheoreticalLines()).to(equal(1))
+                    expect(labelSubject.oneLineLabelWithTruncation.numberOfTheoreticalLines()).to(equal(2))
+                    expect(labelSubject.twoLineLabelNoTruncation.numberOfTheoreticalLines()).to(equal(2))
+                    expect(labelSubject.oneLineLabelWithIncreasingFontToTruncate.numberOfTheoreticalLines()).to(equal(3))
+                    expect(labelSubject.oneLineLabelThatSizeWillDecreaseToTruncate.numberOfTheoreticalLines()).to(equal(2))
+                    expect(labelSubject.threeLineLabelWithTruncation.numberOfTheoreticalLines()).to(equal(5))
                 }
             }
         }
