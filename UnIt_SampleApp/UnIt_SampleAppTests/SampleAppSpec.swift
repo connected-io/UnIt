f6ee7dcaad60c02c8f222895b6f2e751496456ff
Amaan <akhan@connected.io>
2019-07-18 10:59:49 -0400

    Add VCFromNibSpec file.

--- UnIt_SampleApp/UnIt_SampleAppTests/SampleAppSpec.swift
+++ UnIt_SampleApp/UnIt_SampleAppTests/SampleAppSpec.swift
@@ -6,53 +6,11 @@ import UnIt
 
 class SampleAppSpec: QuickSpec {
     override func spec() {
-        var nibSubject : NibViewController!
         var actionSubject : ActionViewController!
         var overlapSubject : OverlapViewController!
         var labelSubject: LabelViewController!
         var constraintBreakSubject: ConstraintBreakViewController!
         var offscreenSubject: OffScreenViewController!
-
-        describe("ViewController from nib") {
-            beforeEach() {
-                nibSubject = UIViewController.loadAndSetupViewControllerFromNib("NibViewController", NibViewController.self, Device.iPhoneXS)
-            }
-
-            it("should not be nil") {
-                expect(nibSubject).notTo(beNil())
-                expect(nibSubject.view).notTo(beNil())
-            }
-
-            it("should have a label that has text: 'Top View Controller Label'") {
-                expect(nibSubject.view.firstLabel(with: "Top View Controller Label")).notTo(beNil())
-
-            }
-
-            it("should have a button that has text: 'Bottom View Controller Button'") {
-                expect(nibSubject.view.firstButton(with: "Bottom View Controller Button")).notTo(beNil())
-
-            }
-
-            it("should not have a label that has text: 'No Label'") {
-                expect(nibSubject.view.firstLabel(with: "No Label")).to(beNil())
-            }
-
-            it("should have a collection view cell with 'Acre' which is a collection view cell inside a view controller that is in a container view") {
-                expect(nibSubject.view.firstVisibleCollectionViewCell(with: "Acre")).notTo(beNil())
-            }
-
-            it("should not have a collection view cell with 'Pernambuco' which is a collection view cell inside a view controller that is in a container view") {
-                expect(nibSubject.view.firstVisibleCollectionViewCell(with: "Pernambuco")).to(beNil())
-            }
-
-            it("should have a label with the text 'Left Stack Label' in a UIStackView") {
-                expect(nibSubject.view.firstLabel(with: "Left Stack Label")).notTo(beNil())
-            }
-
-            it("should have a button with the text 'Right Stack Button' in a UIStackView") {
-                expect(nibSubject.view.firstButton(with: "Right Stack Button")).notTo(beNil())
-            }
-        }
 
         describe("ViewController with user inputs") {
             beforeEach() {
---
+++ UnIt_SampleApp/UnIt_SampleAppTests/VCFromNibSpec.swift
@@ -0,0 +1,60 @@
+//
+//  VCFromNibSpec.swift
+//  UnIt_SampleAppTests
+//
+//  Created by Amaan on 2019-07-18.
+//  Copyright © 2019 cl-dev. All rights reserved.
+//
+
+import Foundation
+import Nimble
+import Quick
+import UnIt
+@testable import UnIt_SampleApp
+
+class VCFromNibSpec: QuickSpec {
+    override func spec() {
+        var nibSubject : NibViewController!
+ 
+        describe("ViewController from nib") {
+            beforeEach() {
+                nibSubject = UIViewController.loadAndSetupViewControllerFromNib("NibViewController", NibViewController.self, Device.iPhoneXS)
+            }
+            
+            it("should not be nil") {
+                expect(nibSubject).notTo(beNil())
+                expect(nibSubject.view).notTo(beNil())
+            }
+            
+            it("should have a label that has text: 'Top View Controller Label'") {
+                expect(nibSubject.view.firstLabel(with: "Top View Controller Label")).notTo(beNil())
+                
+            }
+            
+            it("should have a button that has text: 'Bottom View Controller Button'") {
+                expect(nibSubject.view.firstButton(with: "Bottom View Controller Button")).notTo(beNil())
+                
+            }
+            
+            it("should not have a label that has text: 'No Label'") {
+                expect(nibSubject.view.firstLabel(with: "No Label")).to(beNil())
+            }
+            
+            it("should have a collection view cell with 'Acre' which is a collection view cell inside a view controller that is in a container view") {
+                expect(nibSubject.view.firstVisibleCollectionViewCell(with: "Acre")).notTo(beNil())
+            }
+            
+            it("should not have a collection view cell with 'Pernambuco' which is a collection view cell inside a view controller that is in a container view") {
+                expect(nibSubject.view.firstVisibleCollectionViewCell(with: "Pernambuco")).to(beNil())
+            }
+            
+            it("should have a label with the text 'Left Stack Label' in a UIStackView") {
+                expect(nibSubject.view.firstLabel(with: "Left Stack Label")).notTo(beNil())
+            }
+            
+            it("should have a button with the text 'Right Stack Button' in a UIStackView") {
+                expect(nibSubject.view.firstButton(with: "Right Stack Button")).notTo(beNil())
+            }
+        }
+    }
+}
